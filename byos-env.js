896d3089bf4bf56cd02cf25be4d6afe63f30c15e
ashfame <ashfame@users.noreply.github.com>
2026-05-26 09:19:41 +0000

    Deploy BYOS storage browser sample

---
+++ byos-env.js
@@ -0,0 +1,10 @@
+window.__BYOS_CONFIG__ = {
+  VITE_BYOS_AUTH_BASE_URL: "http://127.0.0.1:8084",
+  VITE_BYOS_OAUTH_CLIENT_ID: "",
+  VITE_BYOS_OAUTH_REDIRECT_URI: "https://ashfame.github.io/byos-storage-browser-demo/callback",
+  VITE_BYOS_OAUTH_SCOPES: "storage:app storage:s3",
+  VITE_BYOS_S3_ENDPOINT: "http://127.0.0.1:8083",
+  VITE_BYOS_S3_PREFIX: "",
+  VITE_BYOS_AWS_ACCOUNT_ID: "",
+  VITE_BYOS_PROTOCOL_CREDENTIAL_LABEL: "BYOS Storage Browser React",
+};
