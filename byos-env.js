e725a9861409336315d125295cb566d05ccc9565
ashfame <ashfame@users.noreply.github.com>
2026-05-26 09:32:05 +0000

    Configure BYOS demo OAuth client

--- byos-env.js
+++ byos-env.js
@@ -1,6 +1,6 @@
 window.__BYOS_CONFIG__ = {
   VITE_BYOS_AUTH_BASE_URL: "https://byos.ashfame.com",
-  VITE_BYOS_OAUTH_CLIENT_ID: "",
+  VITE_BYOS_OAUTH_CLIENT_ID: "client_FGpTZRDEzuHxjOXbId7USce0",
   VITE_BYOS_OAUTH_REDIRECT_URI: "https://ashfame.github.io/byos-storage-browser-demo/",
   VITE_BYOS_OAUTH_SCOPES: "storage:app storage:s3",
   VITE_BYOS_S3_ENDPOINT: "https://s3.byos.ashfame.com",
